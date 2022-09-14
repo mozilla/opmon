fb0858ae6a7469b5dc7561385cc90987068808e8
Anna Scholtz <anna@scholtzan.net>
2022-09-14 14:58:37 -0700

    Create view for alerts

---
+++ opmon/templates/alerts_view.sql
@@ -0,0 +1,9 @@
+{{ header }}
+
+CREATE OR REPLACE VIEW
+  `{{ gcp_project }}.{{ dataset }}.{{ normalized_slug }}_alerts`
+AS
+SELECT 
+    *
+FROM
+    `{{ gcp_project }}.{{ dataset }}_derived.{{ normalized_slug }}_alerts`
