9187947012abcc31ff368f238ab2b0bb70c45a64
Anna Scholtz <anna@scholtzan.net>
2022-09-22 13:11:12 -0700

    Fix statistic view when looking over build ID period

--- opmon/templates/statistics_view.sql
+++ opmon/templates/statistics_view.sql
@@ -25,8 +25,8 @@ stats AS (
         `{{ gcp_project }}.{{ dataset }}_derived.{{ normalized_slug }}_statistics_v{{ table_version }}`,
         most_recent_date
     WHERE
-        PARSE_DATE('%Y%m%d', CAST(build_id AS STRING)) = DATE_ADD(submission_date, INTERVAL 14 DAY) OR
-        submission_date > DATE_SUB(most_recent, INTERVAL 14 DAY)
+        PARSE_DATE('%Y%m%d', CAST(build_id AS STRING)) = DATE_SUB(submission_date, INTERVAL 14 DAY) OR
+        submission_date = most_recent
 )
 {% endif %}
 
