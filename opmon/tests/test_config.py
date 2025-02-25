26371529cdf72037faacf12550ba15b2018c2c64
Mike Williams <mwilliams@mozilla.com>
2025-02-25 09:08:48 -0500

    fix broken test

--- opmon/tests/test_config.py
+++ opmon/tests/test_config.py
@@ -16,28 +16,44 @@ class TestConfigLoader:
             ["https://github.com/mozilla/metric-hub/tree/main/opmon"]
         )
         assert configs_collection.configs is not None
-        assert len(configs_collection.configs.configs) == len(ConfigLoader.configs.configs)
+        assert len(configs_collection.configs.configs) == len(
+            ConfigLoader.configs.configs
+        )
 
     def test_spec_for_experiment(self):
         experiment = next(
-            c.slug for c in ConfigLoader.configs.configs if isinstance(c.spec, MonitoringSpec)
+            c.slug
+            for c in ConfigLoader.configs.configs
+            if isinstance(c.spec, MonitoringSpec)
         )
         assert ConfigLoader.configs.spec_for_project(experiment) is not None
 
     def test_spec_for_nonexisting_experiment(self):
-        assert ConfigLoader.configs.spec_for_project("non_exisiting") is None
+        assert ConfigLoader.configs.spec_for_project("non_existing") is None
 
     def test_get_nonexisting_outcome(self):
         assert ConfigLoader.configs.spec_for_outcome("non_existing", "foo") is None
 
     def test_get_data_source(self):
-        metric = list(ConfigLoader.configs.definitions[0].spec.metrics.definitions.values())[0]
-        platform = ConfigLoader.configs.definitions[0].platform
+        definition = [
+            d for d in ConfigLoader.configs.definitions if d.platform != "functions"
+        ][0]
+        metric = [
+            m
+            for m in definition.spec.metrics.definitions.values()
+            if m.data_source is not None
+        ][0]
+        platform = definition.platform
 
         assert (
-            ConfigLoader.configs.get_data_source_definition(metric.data_source.name, platform)
+            ConfigLoader.configs.get_data_source_definition(
+                metric.data_source.name, platform
+            )
             is not None
         )
 
     def test_get_nonexisting_data_source(self):
-        assert ConfigLoader.configs.get_data_source_definition("non_existing", "foo") is None
+        assert (
+            ConfigLoader.configs.get_data_source_definition("non_existing", "foo")
+            is None
+        )
