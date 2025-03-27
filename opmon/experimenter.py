6c6a64a73d6b30919eb295f6740e1c9b6ba27752
Mike Williams <mwilliams@mozilla.com>
2025-03-27 15:17:48 -0400

    chore: remove v1 api (normandy) from opmon; use v8 for nimbus

--- opmon/experimenter.py
+++ opmon/experimenter.py
@@ -53,69 +53,7 @@ class Experiment:
 
 
 @attr.s(auto_attribs=True, kw_only=True, slots=True, frozen=True)
-class ExperimentV1:
-    """Experimenter v1 experiment."""
-
-    slug: str  # experimenter slug
-    type: str
-    status: str
-    name: Optional[str]
-    start_date: Optional[dt.datetime]
-    end_date: Optional[dt.datetime]
-    variants: List[Variant]
-    normandy_slug: Optional[str] = None
-    pref_name: Optional[str]
-    firefox_channel: Optional[str]
-
-    @staticmethod
-    def _unix_millis_to_datetime(num: Optional[float]) -> Optional[dt.datetime]:
-        if num is None:
-            return None
-        return dt.datetime.fromtimestamp(num / 1e3, pytz.utc)
-
-    @classmethod
-    def from_dict(cls, d) -> "ExperimentV1":
-        """Create an experiment from a dictionary."""
-        converter = cattr.Converter()
-        converter.register_structure_hook(
-            dt.datetime,
-            lambda num, _: cls._unix_millis_to_datetime(num),
-        )
-        return converter.structure(d, cls)
-
-    def to_experiment(self) -> "Experiment":
-        """Convert to Experiment."""
-        branches = [Branch(slug=variant.slug, ratio=variant.ratio) for variant in self.variants]
-        control_slug = None
-
-        control_slugs = [variant.slug for variant in self.variants if variant.is_control]
-        if len(control_slugs) == 1:
-            control_slug = control_slugs[0]
-
-        return Experiment(
-            normandy_slug=self.normandy_slug,
-            experimenter_slug=self.slug,
-            name=self.name,
-            type=self.type,
-            status=self.status,
-            start_date=self.start_date,
-            end_date=self.end_date,
-            branches=branches,
-            reference_branch=control_slug,
-            app_name="firefox_desktop",
-            app_id="firefox-desktop",
-            boolean_pref=self.pref_name,
-            channel=(
-                Channel(self.firefox_channel.lower())
-                if self.firefox_channel and Channel.has_value(self.firefox_channel.lower())
-                else None
-            ),
-            is_rollout=(self.type == "rollout"),
-        )
-
-
-@attr.s(auto_attribs=True, kw_only=True, slots=True, frozen=True)
-class ExperimentV6:
+class ExperimentV8:
     """Represents a v6 experiment from Experimenter."""
 
     slug: str  # Normandy slug
@@ -140,7 +78,7 @@ class ExperimentV6:
         return self._appId or "firefox-desktop"
 
     @classmethod
-    def from_dict(cls, d) -> "ExperimentV6":
+    def from_dict(cls, d) -> "ExperimentV8":
         """Create an experiment from a dictionary."""
         converter = cattr.GenConverter()
         converter.register_structure_hook(
@@ -197,10 +135,9 @@ class ExperimentCollection:
     experiments: List[Experiment] = attr.Factory(list)
 
     MAX_RETRIES = 3
-    EXPERIMENTER_API_URL_V1 = "https://experimenter.services.mozilla.com/api/v1/experiments/"
 
     # for nimbus experiments
-    EXPERIMENTER_API_URL_V6 = "https://experimenter.services.mozilla.com/api/v6/experiments/"
+    EXPERIMENTER_API_URL_V8 = "https://experimenter.services.mozilla.com/api/v8/experiments/"
 
     # user agent sent to the Experimenter API
     USER_AGENT = "opmon"
@@ -211,30 +148,19 @@ class ExperimentCollection:
     ) -> "ExperimentCollection":
         """Fetch all experiments from Experimenter."""
         session = session or requests.Session()
-        legacy_experiments_json = retry_get(
-            session, cls.EXPERIMENTER_API_URL_V1, cls.MAX_RETRIES, cls.USER_AGENT
-        )
-        legacy_experiments = []
-
-        for experiment in legacy_experiments_json:
-            if experiment["type"] != "rapid":
-                try:
-                    legacy_experiments.append(ExperimentV1.from_dict(experiment).to_experiment())
-                except Exception as e:
-                    logger.exception(str(e), exc_info=e, extra={"experiment": experiment["slug"]})
 
         nimbus_experiments_json = retry_get(
-            session, cls.EXPERIMENTER_API_URL_V6, cls.MAX_RETRIES, cls.USER_AGENT
+            session, cls.EXPERIMENTER_API_URL_V8, cls.MAX_RETRIES, cls.USER_AGENT
         )
         nimbus_experiments = []
 
         for experiment in nimbus_experiments_json:
             try:
-                nimbus_experiments.append(ExperimentV6.from_dict(experiment).to_experiment())
+                nimbus_experiments.append(ExperimentV8.from_dict(experiment).to_experiment())
             except Exception as e:
                 logger.exception(str(e), exc_info=e, extra={"experiment": experiment["slug"]})
 
-        return cls(nimbus_experiments + legacy_experiments)
+        return cls(nimbus_experiments)
 
     def ever_launched(self) -> "ExperimentCollection":
         """Return all experiments that have ever been live."""
